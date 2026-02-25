2d0642a51758713403597773d2db490a0a444f9c
francis-dotcom <femioginos@gmail.com>
2026-02-25 13:37:37 -0500

    layout: 14 file(s)

---
+++ school-brand.js
@@ -0,0 +1,46 @@
+/**
+ * Single source of truth for the school name.
+ * Edit this file only; all pages and the mobile menu will use it.
+ */
+(function () {
+  'use strict';
+
+  window.SCHOOL_BRAND = {
+    line1: 'GRAND-PLUS COLLEGE',
+    line2: 'OF',
+    line3: 'EDUCATION',
+    alt: 'Grand-Plus College of Education',
+    titleSuffix: 'Grand Plus College'
+  };
+
+  function getSchoolBrandHtml() {
+    var b = window.SCHOOL_BRAND;
+    return '<span class="school-name__line1">' + escapeHtml(b.line1) + '</span>' +
+      '<span class="school-name__line2">' + escapeHtml(b.line2) + '</span>' +
+      '<span class="school-name__line3">' + escapeHtml(b.line3) + '</span>';
+  }
+
+  function escapeHtml(text) {
+    var div = document.createElement('div');
+    div.textContent = text;
+    return div.innerHTML;
+  }
+
+  function applyBrand() {
+    var nodes = document.querySelectorAll('[data-school-brand]');
+    nodes.forEach(function (el) {
+      el.innerHTML = getSchoolBrandHtml();
+    });
+    document.querySelectorAll('img.logo-img').forEach(function (img) {
+      img.setAttribute('alt', window.SCHOOL_BRAND.alt);
+    });
+  }
+
+  window.getSchoolBrandHtml = getSchoolBrandHtml;
+
+  if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', applyBrand);
+  } else {
+    applyBrand();
+  }
+})();
